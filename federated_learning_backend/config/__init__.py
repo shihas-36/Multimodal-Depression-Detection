b92a44c091c266a78f250beed4bbc719ac43abbb
shihas-36 <shihashihas36@gmail.com>
2026-05-03 17:25:35 +0530

    Align Django and Celery settings with Railway env vars

--- federated_learning_backend/config/__init__.py
+++ federated_learning_backend/config/__init__.py
@@ -1,2 +1,7 @@
-# config package initialization
+"""Config package initialization."""
+
+from .celery import app as celery_app
+
 default_app_config = 'config.apps.ConfigConfig'
+
+__all__ = ('celery_app',)
--- federated_learning_backend/config/settings.py
+++ federated_learning_backend/config/settings.py
@@ -62,7 +62,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 # Auth
