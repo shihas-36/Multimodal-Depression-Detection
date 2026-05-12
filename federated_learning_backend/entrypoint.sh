d400d6b4e78a0e7bd907daceaaa5dc5130b82d0d
Shihas S <shihashihas36@gmail.com>
2026-05-12 07:54:04 +0530

    Refactor celery beat command with cleanup step
    Remove existing celery beat command and add cleanup step for celerybeat-schedule.

--- federated_learning_backend/entrypoint.sh
+++ federated_learning_backend/entrypoint.sh
@@ -19,8 +19,7 @@ elif [ "$SERVICE" = "worker" ]; then
     celery -A config worker -l info --concurrency=1
 
 elif [ "$SERVICE" = "beat" ]; then
-    celery -A config beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
-
+    rm -f celerybeat-schedule && celery -A config beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler
 else
     echo "Unknown SERVICE: $SERVICE"
     exit 1
